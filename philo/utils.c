ed2b44392ecb96ac715b3e4d1298d3b9beedaed8
aparolar <aparolar@student.42madrid.com>
2021-12-09 19:00:46 +0100

    update

--- philo/utils.c
+++ philo/utils.c
@@ -6,7 +6,7 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 12:30:32 by aparolar          #+#    #+#             */
-/*   Updated: 2021/12/09 12:40:51 by aparolar         ###   ########.fr       */
+/*   Updated: 2021/12/09 18:57:52 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void    show_status(t_philo *philo, char *status)
 {
 	if (!philo->args->dead)
 	{
-		//pthread_mutex_lock(&philo->args->write);
+		pthread_mutex_lock(&philo->args->write);
 		///*putstr_fd no funciona?¿
 		ft_putnbr_fd(timestamp() - philo->args->start, 1);
 		ft_putstr_fd(" ", 1);
@@ -26,6 +26,6 @@ void    show_status(t_philo *philo, char *status)
 		ft_putstr_fd("\n", 1);
 		//*/
 		//printf("%8lu %2d %s\n", timestamp() - philo->args->start, philo->position + 1, status);
-		//pthread_mutex_unlock(&philo->args->write);
+		pthread_mutex_unlock(&philo->args->write);
 	}
 }
